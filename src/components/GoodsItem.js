f3e27dab11990514491d90ffb557d2bd32048fdf
Alexander Sosov <nookat.develop@gmail.com>
2022-06-28 10:41:47 +0300

    Project complete

---
+++ src/components/GoodsItem.js
@@ -0,0 +1,40 @@
+function GoodsItem(props) {
+  const {
+    mainId,
+    displayName,
+    displayDescription,
+    price: { regularPrice: price },
+    displayAssets: [
+      {
+        full_background
+      }
+    ],
+    addToBasket = Function.prototype
+  } = props
+
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img src={full_background} alt={displayName}/>
+      </div>
+      <div className="card-content">
+        <span className="card-title">{displayName}</span>
+        <p>{displayDescription}</p>
+      </div>
+      <div className="card-action">
+        <button className="btn" onClick={() => addToBasket({
+          id: mainId,
+          name: displayName,
+          price
+        })}>Купить
+        </button>
+        <span className="right" style={{ fontSize: '1.8rem' }}>{price} руб.</span>
+      </div>
+    </div>
+  )
+}
+
+export
+{
+  GoodsItem
+}
